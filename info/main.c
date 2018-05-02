0da2af861b4e51fdd4654626e53725e746bcd56f
root <root@ataguiro.ataguiro.org>
2018-05-02 20:58:34 +0200

    update

--- info/main.c
+++ info/main.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/syscalls.h>
+#include <linux/fs_struct.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Adam Taguirov <ataguiro@student.42.fr>");
@@ -11,15 +12,19 @@ MODULE_DESCRIPTION("Hello World module");
 static int __init hello_init(void) {
 	printk(KERN_INFO "Hello World !\n");
 	struct task_struct *cur;
+	char buffer[PATH_MAX];
 
 	for_each_process(cur) {
-		printk("pid   : %ld\n", cur->pid);
+		printk("name  : %s\n", cur->comm);
+		printk("pid   : %d\n", cur->pid);
 		printk("state : %ld\n", cur->state);
 		printk("stack : %p\n", cur->stack);
-		printk("stack : %zu\n", cur->start_time);
+		printk("age   : %llu\n", cur->start_time);
 		printk("child : none\n");
-		printk("ppid  : %ld\n", cur->parent->pid);
-		printk("ppid  : %ld\n", cur->parent->pid);
+		printk("ppid  : %d\n", cur->parent->pid);
+		printk("root  : %s\n", dentry_path_raw(cur->fs->root.dentry, buffer, PATH_MAX));
+		printk("pwd   : %s\n", dentry_path_raw(cur->fs->pwd.dentry, buffer, PATH_MAX));
+		printk("\n");
 	}
 
 	return 0;
